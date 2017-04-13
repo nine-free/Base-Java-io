3ba04017df00feb53073cc97da830c1c213c267c
zhangjifu <zhangjifu@vipkid.com.cn>
2017-04-13 14:18:43 +0800

    init

---
+++ src/main/java/cn/soft1010/io/FileTest.java
@@ -0,0 +1,15 @@
+package cn.soft1010.io;
+
+/**
+ * Created by zhangjifu on 2017/4/13.
+ */
+public class FileTest {
+
+    public static void main(String[] args) {
+        new FileTest().test();
+    }
+
+    private void test() {
+
+    }
+}
